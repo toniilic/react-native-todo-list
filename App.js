9e4b6e1390d0af78762e687a01653dab84391dbf
Toni <toniilicdeveloper@gmail.com>
2021-08-10 18:28:12 +0200

    init

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, StatusBar, FlatList } from "react-native";
+import styled from "styled-components/native";
+import AddInput from "./Components/AddInput";
+import TodoList from "./Components/TodoList";
+import Header from "./Components/Header";
+import Empty from "./Components/Empty";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ const [data, setData] = useState([]);
+
+ const submitHandler = (value) => {
+    setData((prevTodo) => {
+      return [
+        {
+          value: value,
+          key: Math.random().toString(),
+        },
+        ...prevTodo,
+      ];
+    });
+ }
+
+ const deleteItem = (key) => {
+    setData((prevTodo) => {
+      return prevTodo.filter((todo) => todo.key != key);
+    });
+  };
+
+ return (
+      <ComponentContainer>
+        <View>
+          <StatusBar barStyle="light-content" 
+            backgroundColor="midnightblue" />
+        </View>
+
+        <View>
+          <FlatList
+            data={data}
+            ListHeaderComponent={() => <Header />}
+            ListEmptyComponent={() => <Empty />}
+            keyExtractor={(item) => item.key}
+            renderItem={({ item }) => (
+              <TodoList item={item} deleteItem={deleteItem} />
+            )}
+          />
+          <View>
+            <AddInput submitHandler={submitHandler} />
+          </View>
+        </View>
+      </ComponentContainer>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const ComponentContainer = styled.View`
+  background-color: midnightblue;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
